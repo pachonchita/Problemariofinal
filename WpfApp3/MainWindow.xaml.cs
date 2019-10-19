fae4462559d0dff257be463defdf6bf921e7da0d
brisa jamiley jimenez castillo <bjimenez9@ucol.mx>
2019-10-18 19:18:03 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;//Agregamos libreria OleDB
+using System.Data; //Agregamos System.Data
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection con; //Agregamos la conexión
+        DataTable dt;   //Agregamos la tabla
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Conectamos la base de datos a nuestro archivo Access
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\Ventas.mdb";
+            MostrarDatos();
+        }
+        //Mostramos los registros de la tabla
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Kilos";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        //Limpiamos todos los campos
+        private void LimpiaTodo()
+        {
+            txtId.Text = "";
+            txtPrco.Text = "";
+            txtPrba.Text = "";
+            txtKc.Text = "";
+            txtKb.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
+        }
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+
+            if (txtId.Text != "")
+            {
+                if (txtId.IsEnabled == true)
+                {
+                    if (txtPrba.Text != "")
+                    {
+                        cmd.CommandText = "insert into Kilos(Id,Preciodecochinita,Preciodebarbacoa,Kilosdecochinita,Kilosdebarbacoa)" + "Values(" + txtId.Text + ",'" + txtPrco.Text + "','" + txtPrba.Text + "'," + txtKc.Text + ",'" + txtKb.Text + "')";
+                        cmd.ExecuteNonQuery();
+                        MostrarDatos();
+                        MessageBox.Show("Pedido agregado correctamente...");
+                        LimpiaTodo();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar el  el precio de la barbacoa....");
+                    }
+                }
+                else
+                {
+                    cmd.CommandText = "update Kilos set Preciodecochinita='" + txtPrco.Text + "',Preciodebarbacoa='" + txtPrba.Text + "',Kilosdecochinita=" + txtKc.Text
+                        + ",Kilosdebarbacoa='" + txtKb.Text + "' where Id=" + txtId.Text;
+                    cmd.ExecuteNonQuery();
+                    MostrarDatos();
+                    MessageBox.Show("Datos del pedido Actualizados...");
+                    LimpiaTodo();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor de poner el ID del pedido.......");
+            }
+        }
+
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtId.Text = row["Id"].ToString();
+                txtPrco.Text = row["Preciodecochinita"].ToString();
+                txtPrba.Text = row["Preciodebarbacoa"].ToString();
+                txtKc.Text = row["Kilosdecochinita"].ToString();
+                txtKb.Text = row["Kilosdebarbacoa"].ToString();
+                txtId.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor seleccionar un pedido de la lista");
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                cmd.Connection = con;
+                cmd.CommandText = "delete from Kilos where Id=" + row["Id"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Pedido ELiminado correctamente");
+                LimpiaTodo();
+            }
+            else
+            {
+                MessageBox.Show("Favor seleccionar un pedido de la lista");
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            LimpiaTodo();
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
